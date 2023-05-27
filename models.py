810452238819aa37ad26c210ac38e53412427be0
Michael Friedrich <michael.friedrich@meta.com.br>
2023-05-27 20:43:02 -0300

    created db models

---
+++ models.py
@@ -0,0 +1,15 @@
+from database import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    age = db.Column(db.Integer)
+    email = db.Column(db.String(50))
+    admin = db.Column(db.Boolean)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    content = db.Column(db.String(50))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref=db.backref('posts', lazy=True))
